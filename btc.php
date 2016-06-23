e0cf490881f4e3b668a5a59598059546774b6e9d
papabless <papabless@users.noreply.github.com>
2016-06-23 17:46:13 +0700

    Delete btc.php

--- btc.php
+++
@@ -1,8 +0,0 @@
-<?php
-$intValue = 0;
-$arrResult = json_decode(file_get_contents('http://preev.com/pulse/units:btc+usd/sources:bitfinex+bitstamp+btce'), true);
-    foreach($arrResult['btc']['usd'] as $key => $index){
-        $intValue = $intValue + $arrResult['btc']['usd'][$key]['last'];
-    }
-echo ($intValue / 3);
-?>
